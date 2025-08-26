daabdd52c1ea4ddf5d70b36d13dcedf6801f992f
Marcos Allan <marcosallanm1to@gmail.com>
2025-08-26 17:35:10 -0300

    feat: add User entity

---
+++ src/main/java/com/omarcosallan/spring_security_jwt/entity/User.java
@@ -0,0 +1,34 @@
+package com.omarcosallan.spring_security_jwt.entity;
+
+import com.omarcosallan.spring_security_jwt.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
---
+++ src/main/java/com/omarcosallan/spring_security_jwt/entity/enums/Role.java
@@ -0,0 +1,6 @@
+package com.omarcosallan.spring_security_jwt.entity.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
